c5d768895778a46b375d650fceb75d02e37ea387
ChobotarCostyantin <chobotar.kostiantyn@chnu.edu.ua>
2024-11-03 23:08:21 +0200

    Footer design, fixed authors

--- src/components/layout/AppFooter.tsx
+++ src/components/layout/AppFooter.tsx
@@ -2,7 +2,7 @@ const AppFooter = () => {
   return (
     <footer className="layout-nav-element d-flex flex-wrap justify-content-between align-items-center border-top footer">
       <p className="col-md-4 mb-0 ms-5 fs-5">© 2024 CHNU Students project.</p>
-      <div className="col-md-6 d-flex align-items-center me-3">
+      <div className="d-flex align-items-center me-5">
         <h5 className="mb-1">Authors:</h5>
         <ul className="nav d-flex flex-row text-center">
           <li className="nav-item mb-1 mx-3 fs-5">
