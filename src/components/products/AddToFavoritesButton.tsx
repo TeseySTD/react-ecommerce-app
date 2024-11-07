cf08f582fc4d7d2476a7b102f413a3ca81e75739
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2024-11-07 11:44:28 +0200

    changed animation of add to favorites button and make it on hover with timeout.

--- src/components/products/AddToFavoritesButton.tsx
+++ src/components/products/AddToFavoritesButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import StorageService from '../../utils/storage-service';
 import favorite from '../../assets/heart-thin-icon.svg';
 import favoriteFill from '../../assets/heart-icon.svg';
@@ -12,6 +12,9 @@ const AddToFavoritesButton = (props: AddToFavoritesButtonProps) => {
   const [inFavorites, setInFavorites] = useState(
     StorageService.isFavorite(props.product.id)
   );
+
+  const refFavorites = useRef<HTMLDivElement>(null);
+
   const handleAddToFavorites = () => {
     if (inFavorites) {
       StorageService.removeFavorite(props.product.id);
@@ -22,14 +25,24 @@ const AddToFavoritesButton = (props: AddToFavoritesButtonProps) => {
     }
   };
 
+  const animationHandler = () =>{
+    if(refFavorites.current && !refFavorites.current.classList.contains('animated')){
+      refFavorites.current.classList.toggle('animated');
+
+      setTimeout(() => {
+        refFavorites.current?.classList.toggle('animated');
+      },300)
+    }
+  };
+
   return (
-    <a className="d-flex" onClick={handleAddToFavorites}>
+    <div className="add-favorites-button d-flex" onClick={handleAddToFavorites} onMouseOver={animationHandler} ref={refFavorites}>
       {inFavorites ? (
         <img src={favoriteFill} alt="" style={{ width: '2rem' }} />
       ) : (
         <img src={favorite} alt="" style={{ width: '2rem' }} />
       )}
-    </a>
+    </div>
   );
 };
 
